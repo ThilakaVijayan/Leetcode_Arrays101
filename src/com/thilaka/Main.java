28e28a8d288d7606e7f8da2b087d79fee8688fa7
Thilaka Vijayan <v.thilaka@gmail.com>
2022-04-27 07:53:31 +1000

    Max Consecutive Ones

--- src/com/thilaka/Main.java
+++
@@ -1,8 +0,0 @@
-package com.thilaka;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/com/thilaka/introduction/A_Max_Consecutive_Ones.java
@@ -0,0 +1,27 @@
+package com.thilaka.introduction;
+
+public class A_Max_Consecutive_Ones {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int counter = 0;
+        int maxCounter = 0;
+        // for (int a : nums) {//- 38.60% rating in runtime and 36.75% rating in memory - 1
+        for (int i = 0, numsLength = nums.length; i < numsLength; i++) { //85.35% rating in runtime and 88.45% rating in memory - 1
+            if (nums[i] == 1) {
+                counter++;// - 2
+                //maxCounter = Math.max(maxCounter, ++counter); //85.35% rating in runtime and 88.45% rating in memory - 2
+
+            } else {
+                maxCounter = Math.max(maxCounter, counter);//100% rating in runtime and 81.71% rating in memory - 2
+                counter = 0;
+            }
+        }
+        maxCounter = Math.max(maxCounter, counter);// - 2
+        return maxCounter;
+    }
+
+    public static void main(String[] args) {
+        A_Max_Consecutive_Ones a = new A_Max_Consecutive_Ones();
+        a.findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1});
+    }
+}
