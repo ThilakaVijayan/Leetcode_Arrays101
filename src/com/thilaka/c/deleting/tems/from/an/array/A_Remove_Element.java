b486616c6805cb73f25fb8c711410cc9a3148457
Thilaka Vijayan <v.thilaka@gmail.com>
2022-05-04 21:25:08 +1000

    Remove Element

---
+++ src/com/thilaka/c/deleting/tems/from/an/array/A_Remove_Element.java
@@ -0,0 +1,23 @@
+package com.thilaka.c.deleting.tems.from.an.array;
+
+import java.util.Arrays;
+
+public class A_Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int currIndex=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i] == val){
+
+            }else{
+                nums[currIndex++] = nums[i];
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return currIndex;
+    }
+
+    public static void main(String[] args) {
+        A_Remove_Element a = new A_Remove_Element();
+        System.out.println(a.removeElement(new int[]{0,1,2,2,3,0,4,2},2));
+    }
+}
