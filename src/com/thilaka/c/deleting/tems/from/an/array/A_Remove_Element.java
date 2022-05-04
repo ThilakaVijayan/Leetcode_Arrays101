2d5eacb671c347e43349956d14001dd6396749e4
Thilaka Vijayan <v.thilaka@gmail.com>
2022-05-04 21:36:08 +1000

    Remove Duplicates from Sorted Array

--- src/com/thilaka/c/deleting/tems/from/an/array/A_Remove_Element.java
+++ src/com/thilaka/c/deleting/tems/from/an/array/A_Remove_Element.java
@@ -6,9 +6,7 @@ public class A_Remove_Element {
     public int removeElement(int[] nums, int val) {
         int currIndex=0;
         for(int i=0; i<nums.length;i++){
-            if(nums[i] == val){
-
-            }else{
+            if(nums[i] != val){
                 nums[currIndex++] = nums[i];
             }
         }
