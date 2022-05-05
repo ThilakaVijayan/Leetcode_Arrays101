f647ec6cd99f624f109ef15e33f6524de35316b4
Thilaka Vijayan <v.thilaka@gmail.com>
2022-05-05 12:12:15 +1000

    Remove Duplicates from Sorted Array

---
+++ src/com/thilaka/e/in/place/operations/B_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,21 @@
+package com.thilaka.e.in.place.operations;
+
+import java.util.Arrays;
+
+public class B_Remove_Duplicates_from_Sorted_Array {
+    public int removeDuplicates(int[] nums) {
+        int currPosition = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1] != nums[i]){
+                nums[currPosition++] = nums[i];
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return currPosition;
+    }
+
+    public static void main(String[] args) {
+        B_Remove_Duplicates_from_Sorted_Array b = new B_Remove_Duplicates_from_Sorted_Array();
+        System.out.println(b.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+    }
+}
